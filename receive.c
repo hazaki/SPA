6e6209993d4e17529ddba01e26e500484393ebab
root <root@bt.foo.org>
2015-01-15 19:01:49 +0100

    packet reception with pcap

--- receive.c
+++ receive.c
@@ -1,60 +1,60 @@
 #include <pcap.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 void callback(u_char *user,const struct pcap_pkthdr *h, const u_char *buff)
 {
 	printf("J'ai reçu quelque chose !\n");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	char errbuf[PCAP_ERRBUF_SIZE];
-	char *dev=NULL,*buff=NULL;
-	pcap_t *pdes;
-	bpf_u_int32 net,mask;
-	struct bpf_program bp;
-	char filter[350];
-	int optch;
+	pcap_t *handle;			/* Session handle */
+	char *dev;			/* The device to sniff on */
+	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+	struct bpf_program fp;		/* The compiled filter */
+	char filter_exp[] = "";		/* The filter expression */
+	bpf_u_int32 mask;		/* Our netmask */
+	bpf_u_int32 net;		/* Our IP */
+	struct pcap_pkthdr header;	/* The header that pcap gives us */
+	u_char *packet;			/* The actual packet */
 
-    if((dev=pcap_lookupdev(errbuf)) == NULL)
-    {
-		fprintf(stderr,"unable de detect device : %s\n",errbuf);
-		exit(-1);
-    }
-	printf("using %s as device for sniffing\n",dev);
+	/* Define the device */
+	dev = pcap_lookupdev(errbuf);
+	if (dev == NULL) {
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return(2);
+	}
 
-	if(pcap_lookupnet(dev,&net,&mask,errbuf)==-1)
+	/* Find the properties for the device */
+	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+		net = 0;
+		mask = 0;
+	}
+
+	/* Open the session in promiscuous mode */
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return(2);
+	}
+
+	/* Compile and apply the filter */
+	if (pcap_compile(handle, &fp, filter_exp, 0, mask) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+
+	if (pcap_setfilter(handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+
+	if(pcap_loop(handle,-1,callback,packet)<0)
 	{
-		fprintf(stderr,"unable to lookup net and mask : %s\n",errbuf);
-		exit(-1);
+		fprintf(stderr,"pcap_loop : %s\n",pcap_geterr(handle));
+		return(2);
 	}
-
-    if((pdes=pcap_open_live(dev,1514,IFF_PROMISC,1000,errbuf)) == NULL)
-    {
-	    fprintf(stderr,"unable to open descriptor : %s\n",errbuf);
-	    exit(-1);
-	}
-	
-	if(pcap_compile(pdes,&bp,filter,0x100,mask)<0) 
-	{
-		fprintf(stderr,"compile error : %s\n",pcap_geterr(pdes));
-		exit(-1);
-	}
-
-	if(pcap_setfilter(pdes,&bp)<0) 
-	{
-		fprintf(stderr,"unable to set filter : %s\n",pcap_geterr(pdes));
-		exit(-1);
-	}
-
-	if(pcap_loop(pdes,-1,callback,buff)<0) 
-	{
-		fprintf(stderr,"pcap_loop : %s\n",pcap_geterr(pdes));
-		exit(-1);
-	}
-	
-	return 0;
+	return(0);
 }
+
