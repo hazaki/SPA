3fb8d726d391b12bd34b5558a9df90639721e8df
Jacques Monin <jacmonin@emi.u-bordeaux1.fr>
2015-01-23 11:03:49 +0100

    IP_H uses TPC (forgery works) + filtering on port with pcap)

--- forgery.c
+++ forgery.c
@@ -2,21 +2,29 @@
 #include <libnet.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char ** argv)
 {
-  
+
+    // Vérfication des arguments
+
+    if(argc != 3)
+    {
+        printf("Usage : ./forgery interface ip_dest\n");
+        return(-1);
+    }
+
     libnet_t *l = NULL;
     char errbuf[LIBNET_ERRBUF_SIZE];
-    l = libnet_init(LIBNET_RAW4,"eth0",errbuf);
+    l = libnet_init(LIBNET_RAW4,argv[1],errbuf);
     if (l == NULL){
         libnet_destroy(l);
         printf("Erreur descr : %s\n", errbuf);
         return -1;}
-        
+
     // Création de l'entête UDP
     
     libnet_ptag_t udp = 0;
-    u_int32_t dst_ip = libnet_name2addr4(l,"192.168.128.1",LIBNET_DONT_RESOLVE);
+    u_int32_t dst_ip = libnet_name2addr4(l,argv[2],LIBNET_DONT_RESOLVE);
     if (dst_ip == -1)
     {
         printf("Erreur ip dst\n");
@@ -25,8 +33,8 @@ int main()
     }
     
     udp = libnet_build_udp(
-      54, /* source port */
-      53, /* destination port */
+      12345, /* source port */
+      12346, /* destination port */
       LIBNET_UDP_H + LIBNET_UDP_DNSV4_H /*add the payload's length*/, /* packet length */
       0, /* checksum */
       NULL, /* payload */
@@ -44,8 +52,8 @@ int main()
     
     libnet_ptag_t ip = 0;
     ip = libnet_autobuild_ipv4(
-        LIBNET_IPV4_H + LIBNET_TCP_H,
-        IPPROTO_TCP,
+        LIBNET_IPV4_H + LIBNET_UDP_H,
+        IPPROTO_UDP,
         dst_ip,
         l);
     if (ip == -1)
@@ -55,13 +63,13 @@ int main()
         return -1;
     }
     /*On envoie le paquet */
-    while(1){
+
     libnet_write(l);
-    }
+
     printf("Envoyé\n");
+
     /*On détruit le descripteur*/
     libnet_destroy(l);
- 
+
     return 0;
- 
 }
--- receive.c
+++ receive.c
@@ -8,14 +8,14 @@ void callback(u_char *user,const struct pcap_pkthdr *h, const u_char *buff)
 
 int main(int argc, char *argv[])
 {
-	pcap_t *handle;			/* Session handle */
-	char *dev;			/* The device to sniff on */
-	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-	struct bpf_program fp;		/* The compiled filter */
-	char filter_exp[] = "";		/* The filter expression */
-	bpf_u_int32 mask;		/* Our netmask */
-	bpf_u_int32 net;		/* Our IP */
-	struct pcap_pkthdr header;	/* The header that pcap gives us */
+	pcap_t *handle;			  /* Session handle */
+	char *dev;			  /* The device to sniff on */
+	char errbuf[PCAP_ERRBUF_SIZE];	  /* Error string */
+	struct bpf_program fp;		  /* The compiled filter */
+	char filter_exp[] = "port 12346"; /* The filter expression */
+	bpf_u_int32 mask;		  /* Our netmask */
+	bpf_u_int32 net;		  /* Our IP */
+	struct pcap_pkthdr header;	  /* The header that pcap gives us */
 	u_char *packet;			/* The actual packet */
 
 	/* Define the device */
